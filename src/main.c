7b18560330d836797e0933b36f4d69bbe6650cff
Igor <igorp.lopes16@gmail.com>
2020-06-19 20:03:14 -0300

    Criada a função que trata as entradas do programa

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /* Função que identifica se o número é primo, retornando o número se ele for 
    primo e -1 se ele não for primo*/
@@ -34,7 +36,40 @@ int checarSePrimo(int numero)
 
 }
 
+/* Função que recebe a string de entrada com os números a serem análisados e extrai desta 
+   cada número e os salva em um vetor */
+int separarEmNumeros(int **numeros)
+{
+
+  char entrada[300]; // Armazena a entrada do programa como uma string
+  char *p;
+  int tam = 0; // Tamanho do vetor que armazena os números
+
+  fgets(entrada, sizeof(entrada), stdin); // Recebemos a string de entrada
+
+  // Extraímos os números da string
+  p = strtok(entrada, " \n");
+  for (tam = 0; p != NULL; tam++)
+  {
+    printf ("\n%s\n",p);
+
+    *numeros = (int *) realloc(*numeros, (sizeof(int) * (tam + 1) ) ); // Alocamos mais espaço para o vetor
+
+    (*numeros)[tam] = atoi(p); // Salvamos no vetor o número convertido para int
+
+    p = strtok (NULL, " \n");
+
+  }
+  
+  return tam;
+
+}
+
 int main()
 {
+  int *numeros = NULL; // Vetor que armazena os números a serem avaliados
+  int totNums = 0; // Total de números a serem avaliados
+
+  totNums = separarEmNumeros(&numeros);
   return 0;
 }
